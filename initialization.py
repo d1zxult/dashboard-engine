3d0490c1b60185e2e1756df6419eb9d673f51425
Артём Васин <artvas.00@inbox.ru>
2024-10-12 18:51:14 +0300

    feat/refactor/fix UX/UI fixes and some improvements

--- initialization.py
+++ initialization.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from configuration import create_app
 
-app, resources_handler, compatibility_core = create_app()
+app, resources_handler = create_app()
 
 @app.route('/')
 def index():
